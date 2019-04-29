add8fb6857505aa2b9a645c48c42eb235eba503a
Kehinde Omotoso <kehindejohnomotoso@gmail.com>
2019-04-29 16:36:35 +0100

    First Commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "../../images/Wedeyy Heritage.png";
+import logo from "../../images/WedeyyHeritage.png";
 
 const Logo = props => (
   <img
     src={logo}
     width={props.width}
     height={props.height}
-    alt="Wedeyy Connect"
+    alt="Wedeyy Heritage"
+    className="Logo"
   />
 );
 
